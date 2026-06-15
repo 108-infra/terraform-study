c337be0b5cd9d1bec0777d616637105b5cacb834
108-infra <293319097+108-infra@users.noreply.github.com>
2026-06-15 18:53:26 +0900

    feat: add ECR module

---
+++ modules/ecr/main.tf
@@ -0,0 +1,32 @@
+resource "aws_ecr_repository" "this" {
+  name                 = var.repository_name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = var.tags
+}
+
+# 古いイメージを自動削除するライフサイクルポリシー
+resource "aws_ecr_lifecycle_policy" "this" {
+  repository = aws_ecr_repository.this.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "最新5世代のみ保持"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 5
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
